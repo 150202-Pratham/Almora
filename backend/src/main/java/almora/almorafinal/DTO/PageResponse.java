8aa8898c33de7332d81dc5b9c6979de16f553d63
Pratham <prathamgarg1502@gmail.com>
2026-09-13 22:26:17 +0530

    Added PageResponse DTO to stop returning direct Product object nstead mapping it to PageResponse DTO

--- backend/src/main/java/almora/almorafinal/DTO/PageResponse.java
+++ backend/src/main/java/almora/almorafinal/DTO/PageResponse.java
@@ -1,4 +1,19 @@
 package almora.almorafinal.DTO;
 
-public class PageResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class PageResponse<T> {
+
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean first;
+    private boolean last;
 }
