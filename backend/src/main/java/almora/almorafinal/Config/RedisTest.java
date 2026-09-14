358c23b1041b6f900f0b0a09a1b3f6698b32ddd2
Pratham <prathamgarg1502@gmail.com>
2026-09-14 22:53:57 +0530

    Installed and Configured Redis

--- backend/src/main/java/almora/almorafinal/Config/RedisTest.java
+++ backend/src/main/java/almora/almorafinal/Config/RedisTest.java
@@ -1,4 +1,25 @@
 package almora.almorafinal.Config;
 
-public class RedisTest {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisTest implements CommandLineRunner {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public RedisTest(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void run(String... args) {
+
+        redisTemplate.opsForValue().set("almora:test", "Redis is working");
+
+        String value = redisTemplate.opsForValue().get("almora:test");
+
+        System.out.println("Redis test: " + value);
+    }
 }
